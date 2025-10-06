b6cc62def3d64eb9a737ce57512154e76f2309d0
abror <abror@MacBook-Pro-2.local>
2025-10-06 21:02:37 +0500

    code style

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -17,59 +17,59 @@ import java.util.Properties;
 
 public class Util {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/testdb?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASS = "root";
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+private static final String URL = "jdbc:mysql://localhost:3306/testdb?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+private static final String USER = "root";
+private static final String PASS = "root";
+private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
-    private Connection connection = null;
-    private SessionFactory sessionFactory = null;
+private Connection connection = null;
+private SessionFactory sessionFactory = null;
 
 
-    public Connection getConnection() {
+public Connection getConnection() {
+    try {
+        connection = DriverManager.getConnection(URL, USER, PASS);
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return connection;
+}
+
+
+private static Properties getProperties() {
+    Properties properties = new Properties();
+
+    properties.put(Environment.DRIVER, DRIVER);
+    properties.put(Environment.URL, URL);
+    properties.put(Environment.USER, USER);
+    properties.put(Environment.PASS, PASS);
+    properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+    properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+    properties.put(Environment.SHOW_SQL, "true");
+    properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+    properties.put(Environment.FORMAT_SQL, "true");
+    properties.put(Environment.USE_SQL_COMMENTS, "true");
+    return properties;
+}
+
+public SessionFactory getSessionFactory() {
+    if (sessionFactory == null) {
         try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-        } catch (SQLException e) {
+            Configuration configuration = new Configuration();
+            configuration.setProperties(getProperties());
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                    .applySettings(getProperties()).build();
+            Metadata metadata = new MetadataSources(registry)
+                    .addAnnotatedClass(User.class)
+                    .buildMetadata();
+            sessionFactory = metadata.buildSessionFactory();
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return connection;
     }
-
-
-    private static Properties getProperties() {
-        Properties properties = new Properties();
-
-        properties.put(Environment.DRIVER, DRIVER);
-        properties.put(Environment.URL, URL);
-        properties.put(Environment.USER, USER);
-        properties.put(Environment.PASS, PASS);
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        properties.put(Environment.HBM2DDL_AUTO, "create-drop");
-        properties.put(Environment.SHOW_SQL, "true");
-        properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-        properties.put(Environment.FORMAT_SQL, "true");
-        properties.put(Environment.USE_SQL_COMMENTS, "true");
-        return properties;
-    }
-
-    public SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-                configuration.setProperties(getProperties());
-                configuration.addAnnotatedClass(User.class);
-
-                ServiceRegistry registry = new StandardServiceRegistryBuilder()
-                        .applySettings(getProperties()).build();
-                Metadata metadata = new MetadataSources(registry)
-                        .addAnnotatedClass(User.class)
-                        .buildMetadata();
-                sessionFactory = metadata.buildSessionFactory();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return sessionFactory;
-    }
+    return sessionFactory;
+}
 }
